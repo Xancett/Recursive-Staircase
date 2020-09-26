5f073e23d5427da96f04e2e224435a977c9db2e6
Xancett <ryan.j@epoxstudios.com>
2020-09-26 10:53:58 -0400

    Setup java docs

--- src/Stairs.java
+++ src/Stairs.java
@@ -1,4 +1,19 @@
 
+/**
+ * @author Ryan James
+ *
+ */
 public class Stairs {
-
+	
+	/**
+	 * Returns the number of combinations to take stairs for n steps
+	 * @param n steps
+	 * @return
+	 */
+	public int num_steps(int n) {
+		return 0;
+	}
+	public int num_steps(int n, int[] x) {
+		return 0;
+	}
 }
