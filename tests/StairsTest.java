4abf1ea401f2c4035ab47342be5cb930d4f7a88b
Xancett <ryan.j@epoxstudios.com>
2020-09-26 10:53:45 -0400

    Created tests for first method

--- tests/StairsTest.java
+++ tests/StairsTest.java
@@ -4,13 +4,66 @@ import org.junit.jupiter.api.Test;
 
 class StairsTest {
 
+	//================================================================================
+    // First method test
+    //================================================================================
+	
+	@Test
+	void testNum1(){
+		Stairs stairs = new Stairs();
+		int n = 1;
+		int result = stairs.num_steps(n);
+		int expected = 1;
+		assertEquals(result, expected);
+	}
+	
 	@Test
 	void testNum2() {
 		Stairs stairs = new Stairs();
 		int n = 2;
-		int result = stairs.num_steps(2);
+		int result = stairs.num_steps(n);
+		int expected = 2;
+		assertEquals(result, expected);
+	}
+	
+	@Test
+	void testNum3() {
+		Stairs stairs = new Stairs();
+		int n = 3;
+		int result = stairs.num_steps(n);
+		int expected = 3;
+		assertEquals(result, expected);
+	}
+	
+	@Test
+	void testNum4() {
+		Stairs stairs = new Stairs();
+		int n = 4;
+		int result = stairs.num_steps(n);
+		int expected = 6;
+		assertEquals(result, expected);
+	}
+	
+	@Test
+	void testNum0() {
+		Stairs stairs = new Stairs();
+		int n = 0;
+		int result = stairs.num_steps(n);
 		int expected = 0;
 		assertEquals(result, expected);
 	}
-
+	
+	@Test
+	void testNumNeg4() {
+		Stairs stairs = new Stairs();
+		int n = -4;
+		int result = stairs.num_steps(n);
+		int expected = -1;
+		assertEquals(result, expected);
+	}
+	
+	
+	//================================================================================
+    // Second method test
+    //================================================================================
 }
