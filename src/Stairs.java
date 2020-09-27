cb5bb110efc912885531464e00bdff7e1d77bd84
Xancett <ryan.j@epoxstudios.com>
2020-09-27 11:34:24 -0400

    Initial working

--- src/Stairs.java
+++ src/Stairs.java
@@ -11,7 +11,19 @@ public class Stairs {
 	 * @return
 	 */
 	public int num_steps(int n) {
-		return 0;
+		// Initial check for more than 0 steps
+		if (n < 0) return -1;
+		// Check for 0 steps
+		if (n == 0) return 0;
+		// Check for steps
+		if (n < 2) {
+			return (int)Math.pow(1, n - 2);
+		}
+		if (n == 2) {
+			return (int)(Math.pow(1, n - 2) + Math.pow(2, n - 2));
+		}
+		int totalCount = num_steps(n - 1) + num_steps(n - 2);
+		return totalCount;
 	}
 	public int num_steps(int n, int[] x) {
 		return 0;
